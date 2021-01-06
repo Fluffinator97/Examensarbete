87b34c3fb4c23525f5936a0904018b2fbd40ff89
Fluffinator97 <philip.arvidsson97@gmail.com>
2021-01-06 14:22:20 +0100

    diagram

---
+++ src/components/mobileview/diagrams/DoubleCircleChart.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { PieChart, Pie, ResponsiveContainer, Cell, Legend } from "recharts";
+
+function DoubleCircleDiagram() {
+
+const data = [
+  { name: 'Work Left', value: Math.floor(Math.random() * 100)},
+  { name: 'Work Done', value: Math.floor(Math.random() * 100) },
+];
+
+const COLORS = ['#f90909', '#4ad43d'];
+
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({
+  cx, cy, midAngle, innerRadius, outerRadius, percent, index,
+}) => {
+   const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
+
+    return (
+      <ResponsiveContainer width="100%" height={250}>
+      <PieChart >
+        <Legend />
+        <Pie
+          data={data}
+          cx="50%"
+          cy="50%"
+          labelLine={false}
+          label={renderCustomizedLabel}
+          outerRadius={80}
+          fill="#8884d8"
+          dataKey="value"
+        >
+          {
+            data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
+          }
+        </Pie>
+      </PieChart>
+      </ResponsiveContainer>
+    );
+  }
+
+export default DoubleCircleDiagram;
